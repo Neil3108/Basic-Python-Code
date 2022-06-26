5123de030eeba473340f6879268aa1d4d09e355e
Neil3108 <40967818+Neil3108@users.noreply.github.com>
2022-06-26 14:50:07 -0500

    Create calulator.py

---
+++ calulator.py
@@ -0,0 +1,24 @@
+import sys
+import math
+import threading
+import time
+from functools import reduce
+
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+c = input("Enter operations (+,-,*,/,**): ")
+
+match c:
+    case "+":
+        print(int(a)+int(b))
+    case "-":
+        print(int(a) - int(b))
+    case "*":
+        print(int(a) * int(b))
+    case "/":
+        print(int(a) / int(b))
+    case "**":
+        print(int(a) ** int(b))
+    case _:
+        print("Wrong input.")
