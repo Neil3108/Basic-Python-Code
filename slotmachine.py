b3c4545545fc7eb295c209ebde0a46fac07107d9
Neil3108 <40967818+Neil3108@users.noreply.github.com>
2022-06-26 16:18:22 -0500

    Create slotmachine.py

---
+++ slotmachine.py
@@ -0,0 +1,54 @@
+import sys
+import math
+import threading
+import time
+import random
+from functools import reduce
+
+def getSymbols():
+    numSymbol = int(input("Enter the number of symbols: ").strip())
+    symbolList = []
+    counter = 0
+    while True:
+        if numSymbol <= 0:
+            print("The number of symbols must be greater than 0")
+            numSymbol = input("Enter the number of symbols: ")
+        else:
+            break
+    while counter < numSymbol:
+        symbol = input("Enter 1 symbol: ")
+        symbolList.append(symbol)
+        counter += 1
+
+    return symbolList
+
+def generateSequences(symbols):
+    sequences = []
+    for symbols0 in symbols:
+        for symbols1 in symbols:
+            for symbols2 in symbols:
+                sequences.append(symbols0 + symbols1 + symbols2)
+    return sequences
+
+def displaySequences(sequences):
+    print("\nThe sequences of symbols are:")
+    for sequence in sequences:
+        print(sequence)
+
+def countWinners(sequences):
+    countWinners = 0
+    for sequence in sequences:
+        if sequence[0] == sequence[1] and sequence[1] == sequence[2]:
+            countWinners += 1
+    return countWinners
+
+
+def main():
+    symbols = getSymbols()
+    sequences = generateSequences(symbols)
+    displaySequences(sequences)
+    winners = countWinners(sequences)
+
+    print("There are %d winning sequences out of %d total sequences." % (winners, len(sequences)))
+
+main()
