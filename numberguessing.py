79a2b0d7c43f88f76e0832eec94aebdd5d52da97
Neil3108 <40967818+Neil3108@users.noreply.github.com>
2022-06-28 12:55:21 -0500

    Update numberguessing.py

--- numberguessing.py
+++ numberguessing.py
@@ -1,1 +1,24 @@
+import sys
+import math
+import threading
+import time
+import random
+from functools import reduce
 
+
+def main():
+    number = random.randint(1,100) # Random number
+    guess = int(input("Enter your guess: ")) # User's guess
+
+    while number != guess:
+        if guess < number:
+            print("Your guess is too low")
+            guess = int(input("Enter your guess: "))
+        else:
+            print("Your guess is too high")
+            guess = int(input("Enter your guess: "))
+            
+    if number == guess:
+        print("Your guess is correct!")
+
+main()
