9ee8f28d1c680e7582cb75278f2acf9df945411c
Neil3108 <40967818+Neil3108@users.noreply.github.com>
2022-07-01 17:51:59 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -11,29 +11,65 @@ board = {'1':' ', '2':' ', '3':' ',
         '7': ' ','8':' ','9':' '}
 
 def checkWinner():
+    winner = False
     if board['1'] == board['2'] == board['3']:
-        print(board['1'] + "Wins!")
+        print(board['1'] + " Wins!")
+        winner = True
     elif board['4'] == board['5'] == board['6']:
-        print(board['4'] + "Wins!")
+        print(board['4'] + " Wins!")
+        winner = True
     elif board['7'] == board['8'] == board['9']:
-        print(board['7'] + "Wins!")
+        print(board['7'] + " Wins!")
+        winner = True
     elif board['1'] == board['4'] == board['7']:
-        print(board['1'] + "Wins!")
+        print(board['1'] + " Wins!")
+        winner = True
     elif board['2'] == board['5'] == board['8']:
-        print(board['2'] + "Wins!")
+        print(board['2'] + " Wins!")
+        winner = True
     elif board['3'] == board['6'] == board['9']:
-        print(board['3'] + "Wins!")
+        print(board['3'] + " Wins!")
+        winner = True
     elif board['1'] == board['5'] == board['9']:
-        print(board['1'] + "Wins!")
+        print(board['1'] + " Wins!")
+        winner = True
     elif board['3'] == board['5'] == board['7']:
-        print(board['3'] + "Wins!")
+        print(board['3'] + " Wins!")
+        winner = True
+
+    return winner
 
 
-
-def main():
+def printBoard():
     for i in board:
         print(board[i]+"|", end="")
         if int(i)%3 == 0:
             print()
+
+def playGame():
+    counter = 0
+    symbol = 'x'
+    winner = False
+    while counter < 9:
+        printBoard()
+        move = input()
+        counter += 1
+
+        if board[move] == ' ':
+            board[move] = symbol
+        if counter >= 5:
+            winner = checkWinner()
+        if winner:
+            printBoard()
+            break
+        if symbol == 'x':
+            symbol = 'o'
+        else:
+            symbol = 'x'
+    if counter == 9 & winner == False:
+        print("Its a tie!")
+
+def main():
+    playGame()
 
 main()
