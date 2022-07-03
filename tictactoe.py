bb4c2fee42a3ff35b2ac6b42897a953d6918256e
Neil3108 <40967818+Neil3108@users.noreply.github.com>
2022-07-03 17:50:50 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -14,35 +14,27 @@ def checkWinner():
     winner = False
     if (board['1'] == board['2'] == board['3']) and board['1'] != ' ':
         print(board['1'] + " Wins!")
-        print("1")
         winner = True
     elif (board['4'] == board['5'] == board['6']) and board['4'] != ' ':
         print(board['4'] + " Wins!")
-        print("2")
         winner = True
     elif (board['7'] == board['8'] == board['9']) and board['7'] != ' ':
         print(board['7'] + " Wins!")
-        print("3")
         winner = True
     elif (board['1'] == board['4'] == board['7']) and board['1'] != ' ':
         print(board['1'] + " Wins!")
-        print("4")
         winner = True
     elif (board['2'] == board['5'] == board['8']) and board['2'] != ' ':
         print(board['2'] + " Wins!")
-        print("5")
         winner = True
     elif (board['3'] == board['6'] == board['9']) and board['3'] != ' ':
         print(board['3'] + " Wins!")
-        print("6")
         winner = True
     elif (board['1'] == board['5'] == board['9']) and board['1'] != ' ':
         print(board['1'] + " Wins!")
-        print("7")
         winner = True
     elif (board['3'] == board['5'] == board['7']) and board['3'] != ' ':
         print(board['3'] + " Wins!")
-        print("8")
         winner = True
 
     return winner
